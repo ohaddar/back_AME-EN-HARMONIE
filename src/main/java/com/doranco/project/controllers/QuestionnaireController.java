7a48a87475901804f495ba8c856e71bbb03a66bf
Oumaima <oumaima.haddar.majdeb@gmail.com>
2025-01-19 18:07:35 +0100

    ♻️ refactor questionnaire functionalities code

--- src/main/java/com/doranco/project/controllers/QuestionnaireController.java
+++ src/main/java/com/doranco/project/controllers/QuestionnaireController.java
@@ -2,6 +2,7 @@ package com.doranco.project.controllers;
 
 import com.doranco.project.entities.Questionnaire;
 import com.doranco.project.services.QuestionnaireService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +11,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/questionnaire")
 public class QuestionnaireController {
-    private final QuestionnaireService questionnaireService;
+   @Autowired
+    QuestionnaireService questionnaireService;
 
-    public QuestionnaireController(QuestionnaireService questionnaireService) {
-        this.questionnaireService = questionnaireService;
-    }
+
     @PostMapping("/save")
     public ResponseEntity<Questionnaire>   saveQuestionnaire(@RequestBody Questionnaire questionnaire) {
         Questionnaire addedQuestionnaire = questionnaireService.saveQuestionnaire(questionnaire);
-        if (questionnaire == null) {
-            return ResponseEntity.badRequest().body(null);
-        }
-
         return ResponseEntity.ok(addedQuestionnaire);
 
     }
--- src/main/java/com/doranco/project/servicesImp/QuestionnaireServiceImp.java
+++ src/main/java/com/doranco/project/servicesImp/QuestionnaireServiceImp.java
@@ -4,6 +4,7 @@ import com.doranco.project.entities.Questionnaire;
 import com.doranco.project.repositories.IQuestionnaireRepository;
 import com.doranco.project.services.QuestionnaireService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +16,20 @@ public class QuestionnaireServiceImp implements QuestionnaireService {
     IQuestionnaireRepository questionnaireRepository;
     @Override
     public Questionnaire saveQuestionnaire(Questionnaire questionnaire) {
-        return questionnaireRepository.save(questionnaire);
+        if (questionnaire == null) {
+            throw new IllegalArgumentException("Error occurred while saving questionnaire.");
+        }
 
+        return questionnaireRepository.save(questionnaire);
 
     }
     @Override
     public Questionnaire getQuestionnaire () {
-        return questionnaireRepository.findAll().getFirst();
+
+                try{
+                    return questionnaireRepository.findAll().getFirst();
+                }catch (Exception e) {
+                    throw new RuntimeException("Error occurred while fetching public feedbacks.", e);
+                }
     }
 }
